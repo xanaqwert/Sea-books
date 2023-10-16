ee2345745133e1a862999542054a163629f45986
xanaqwert <95111419+xanaqwert@users.noreply.github.com>
2023-10-16 19:25:23 +0700

    add all update

---
+++ berita.php
@@ -0,0 +1,193 @@
+<?php
+require 'includes/snippet.php';
+require 'includes/db-inc.php';
+include "includes/header.php";
+?>
+
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="shortcut icon" href="/assets/images/Logo.png" type="image/x-icon" />
+  <link rel="stylesheet" href="css/berita.css">
+  <title>
+    sea-books
+  </title>
+  <meta name="title" content="Melthy - Layanan Kesehatan Mental" />
+  <meta name="description" content='' />
+</head>
+
+<body>
+  <?php include "includes/nav.php"; ?>
+  <!-- #########################################################################-->
+
+  <!-- *Start main section berita* -->
+  <section class="section-berita">
+    <div class="container">
+      <header class="header-berita" data-reveal="left">
+        <h1 class="heading-1">
+          Kegiatan berbagi bersama di Kecamatan Palmerah
+        </h1>
+        <div class="sub-heading">
+          <p>Kamis, <span>Mei 26, 2023</span></p>
+          <p class="importent">AGENDA MELTHY</p>
+        </div>
+      </header>
+
+      <div class="main">
+        <div class="home">
+          <div class="left" data-reveal="left">
+            <img src="/assets/images/kegiatan/kegiatan1.jpeg" class="home-img" alt="Paper photo" />
+
+            <!-- <h2 class="heading-2">
+                Kecamatan Palmerah, Mei 31, 2023 - Dalam semangat kepedulian dan
+                solidaritas, Melthy serta masyarakat Kecamatan Palmerah bersatu
+                untuk menggelar acara "Berbagi Bersama" yang bertujuan untuk
+                membantu mereka yang membutuhkan.
+              </h2> -->
+          </div>
+
+          <div class="right" data-reveal="right">
+            <h3 class="heading-3">Agenda Terbaru</h3>
+            <div class="lists">
+              <div class="list">
+                <img src="/assets/images/kegiatan/kegiatan2.jpeg" alt="photo 1" />
+                <p>
+                  <a href="/pages/berita2.html">Senam bersama di RPTRA Sukapura</a>
+                </p>
+              </div>
+
+              <div class="list">
+                <img src="/assets/images/kegiatan/kegiatan3.jpeg" alt="photo 2" />
+                <p>
+                  <a href="/pages/berita3.html">Kegiatan bersih bersih bersama di kelurahan Kelapa
+                    Gading</a>
+                </p>
+              </div>
+
+              <div class="list">
+                <img src="/assets/images/kegiatan/kegiatan4.jpeg" alt="photo 3" />
+                <p>
+                  <a href="/pages/berita4.html">Kegiatan Family Gathering di Istana Bogor</a>
+                </p>
+              </div>
+
+              <div class="list">
+                <img src="/assets/images/kegiatan/kegiatan6.jpeg" alt="photo 4" />
+                <p>
+                  <a href="/pages/berita5.html">Ohlaraga Yoga bersama di Fit Lab, Kelapa Gading</a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <article>
+          <p data-reveal>
+            Acara "Berbagi Bersama" tersebut merupakan upaya dari Melthy serta
+            masyarakat Kecamatan Palmerah yang ingin memberikan kontribusi
+            positif bagi sesama di komunitas mereka. Acara ini melibatkan
+            berbagai kegiatan mulai dari bazaar amal, penyuluhan kesehatan
+            mental, donasi makanan dan paket sembako, hingga penggalangan dana
+            untuk mendukung kegiatan sosial di kecamatan tersebut. Salah satu
+            highlight dari acara ini adalah bazaar amal yang menawarkan
+            barang-barang bekas yang masih layak pakai dengan harga terjangkau.
+            Banyak warga Kecamatan Palmerah yang mendonasikan barang-barang
+            mereka untuk dijual, dan hasil penjualan akan disumbangkan untuk
+            mendukung kegiatan sosial di wilayah tersebut. Bazaar amal ini tidak
+            hanya memberikan kesempatan bagi masyarakat untuk berbelanja
+            barang-barang dengan harga terjangkau, tetapi juga mendorong siklus
+            berkelanjutan dengan mengurangi pemborosan dan memanfaatkan barang
+            yang masih bisa digunakan.
+          </p>
+
+          <p data-reveal>
+            Selain itu, para peserta acara juga bisa mendapatkan manfaat dari
+            penyuluhan dan layanan kesehatan yang disediakan. Terdapat area
+            khusus di acara tersebut yang menawarkan pemeriksaan kesehatan
+            gratis, edukasi gaya hidup sehat, serta penyuluhan tentang kesehatan
+            mental. Para tenaga medis dan profesional kesehatan memberikan
+            layanan dengan penuh dedikasi untuk memastikan kesehatan dan
+            kesejahteraan masyarakat Kecamatan Palmerah. Tidak hanya itu, dalam
+            semangat berbagi, masyarakat juga turut berkontribusi dengan
+            menyumbangkan makanan non-perishable dan paket sembako. Donasi ini
+            dikumpulkan dan akan didistribusikan kepada keluarga yang
+            membutuhkan di Kecamatan Palmerah. Kolaborasi dengan yayasan,
+            organisasi sosial, dan lembaga terkait memastikan bahwa bantuan
+            tersebut tersampaikan kepada penerima yang membutuhkan dengan baik.
+          </p data-reveal>
+
+          <p data-reveal>
+            Selain kegiatan berbagi materi, penggalangan dana juga menjadi
+            bagian penting dalam acara ini. Para peserta dan masyarakat umum
+            hadir dengan sukarela untuk memberikan sumbangan demi mendukung
+            kegiatan sosial di Kecamatan Palmerah. Kotak sumbangan disediakan di
+            lokasi acara, dan petugas khusus bertugas untuk mengumpulkan donasi
+            dengan penuh tanggung jawab.
+          </p>
+        </article>
+      </div>
+    </div>
+
+    <blockquote class="quotes" data-reveal>
+      Acara "Berbagi Bersama" ini juga dimeriahkan dengan berbagai hiburan dan
+      kegiatan komunitas. Panggung diisi dengan pertunjukan seni lokal, seperti
+      musik, tari, teater, dan pertunjukan budaya lainnya.
+    </blockquote>
+  </section>
+
+
+  <!-- *Footer* -->
+  <!-- *__________________________Mulai Dari Bagian Footer__________________________* -->
+  <div class="footers">
+    <footer style="padding-top: 5rem;">
+      <!-- <div class="row">
+          <div class="col col1" data-aos="fade-right">
+            <img
+              src="/assets/images/Logo.png"
+              alt="gambar logo moiseum"
+              style="width: 7rem"
+            />
+            <h1>...</h1>
+            <p>MELTHY, YOUR MENTAL HEALTH SOLUTION</p>
+          </div>
+          <div class="col col2" data-aos="fade-right">
+            <ul>
+              <li>
+                <a href="/index.html">Home</a>
+              </li>
+              <li>
+                <a href="/index.html">Perawatan</a>
+              </li>
+              <li>
+                <a href="/index.html">Layanan</a>
+              </li>
+              <li>
+                <a href="/index.html">About</a>
+              </li>
+              <li>
+                <a href="/index.html">Contact</a>
+              </li>
+            </ul>
+          </div>
+          <div class="col col3" data-aos="fade-right">
+            <h3>JADILAH YANG PERTAMA UNTUK MENDENGAR KABAR DARI KAMI</h3>
+            <input type="text" />
+            <span>E-MAIL</span>
+            <button>SUBSCRIBE</button>
+          </div>
+        </div> -->
+    </footer>
+  </div>
+  <!-- *__________________________Akhir Dari Bagian Footer__________________________* -->
+
+
+  <!-- #########################################################################-->
+
+  <!-- *Main JSnya* -->
+  <!-- #########################################################################-->
+</body>
+
+</html>
